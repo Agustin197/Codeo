30f937fb8dedd825ad775bd5d00dfa56e933c32b
agustin <agustin.97.ceballos@gmail.com>
2022-06-25 05:47:59 -0300

    25/06

---
+++ Op_Ternarios.js
@@ -0,0 +1,16 @@
+
+//Operadores Ternarios
+// Es una forma mas reducida de hacer una pregunta,evitandonos el uso del if
+
+let pc = {
+gabinete:"azul",
+cpu:"i7",
+fan:true,
+};
+
+(pc.gabinete === "azul") && (pc.cpu === "I7") ? console.log("tengo todo") : console.log("no tengo nada");
+
+let perro = "kylo";
+let gata = "sashy";
+
+(perro === "Marley") || (gata === "Simba") ? console.log("estoy en casa") : console.log("todavia no llegue")
